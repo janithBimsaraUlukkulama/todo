16f1f0e006ef28092dd9030a45bd284d3b17ea47
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2022-03-13 17:49:34 +0530

    added decorators and swipe color backgrounds

--- app/src/main/java/com/example/todo/GroupTouchHelper.java
+++ app/src/main/java/com/example/todo/GroupTouchHelper.java
@@ -2,12 +2,16 @@ package com.example.todo;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Canvas;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.todo.Adapter.GroupAdapter;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 public class GroupTouchHelper extends ItemTouchHelper.SimpleCallback {
     private GroupAdapter groupAdapter;
@@ -49,9 +53,22 @@ public class GroupTouchHelper extends ItemTouchHelper.SimpleCallback {
             AlertDialog dialog = builder.create();
             dialog.show();
 
-        }else {
+        } else {
             groupAdapter.editGroup(position);
         }
 
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                .addSwipeLeftBackgroundColor(ContextCompat.getColor(groupAdapter.getContext(), R.color.pastel_white))
+                .addSwipeLeftActionIcon(R.drawable.ic_baseline_done_all_24)
+                .addSwipeRightBackgroundColor(ContextCompat.getColor(groupAdapter.getContext(), R.color.pastel_white))
+                .addSwipeRightActionIcon(R.drawable.ic_baseline_delete_24)
+                .create()
+                .decorate();
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
     }
 }
