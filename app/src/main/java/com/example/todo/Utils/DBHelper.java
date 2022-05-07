39a910a8efbb6605e6e03eb0ce98aed3ac863ed7
techbirdssolutions <jbimsaraulukkulama@gmail.com>
2022-05-07 18:02:42 +0530

    integrated resource files

--- app/src/main/java/com/example/todo/Utils/DBHelper.java
+++ app/src/main/java/com/example/todo/Utils/DBHelper.java
@@ -14,6 +14,8 @@ import com.example.todo.Model.TodoGroupModel;
 import java.util.ArrayList;
 import java.util.List;
 
+import anjalee.Model.ToDoModel;
+
 public class DBHelper extends SQLiteOpenHelper {
     private SQLiteDatabase db;
 
@@ -22,6 +24,15 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String GROUP_COL_1 = "id";
     private static final String GROUP_COL_2 = "name";
     private static final String GROUP_COL_3 = "status";
+
+    //    anjalee --------------
+    private static final String TODO_TABLE = "todo";
+    private static final String ID = "id";
+    private static final String TASK = "task";
+    private static final String STATUS = "status";
+    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + " TEXT, "
+            + STATUS + " INTEGER)";
+    //    anjalee --------------
 
 
     public DBHelper(@Nullable Context context) {
@@ -32,13 +43,26 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + GROUP_TABLE_NAME + "(id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT , status INTEGER)");
 
+        //    anjalee --------------
+        sqLiteDatabase.execSQL(CREATE_TODO_TABLE);
+        //    anjalee --------------
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + GROUP_TABLE_NAME);
+
+        //    anjalee --------------
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TODO_TABLE);
+        //    anjalee --------------
+
         onCreate(sqLiteDatabase);
     }
+
+//    public void openDatabase() {
+//        db = this.getWritableDatabase();
+//    }
 
     public void insertGroup(TodoGroupModel group) {
         db = this.getWritableDatabase();
@@ -68,7 +92,7 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     @SuppressLint("Range")
-    public List<TodoGroupModel> getAllGroups(){
+    public List<TodoGroupModel> getAllGroups() {
 
         db = this.getWritableDatabase();
         Cursor cursor = null;
@@ -76,9 +100,9 @@ public class DBHelper extends SQLiteOpenHelper {
 
         db.beginTransaction();
         try {
-            cursor = db.query(GROUP_TABLE_NAME , null , null , null , null , null , null);
-            if (cursor !=null){
-                if (cursor.moveToFirst()){
+            cursor = db.query(GROUP_TABLE_NAME, null, null, null, null, null, null);
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
                     do {
                         TodoGroupModel group = new TodoGroupModel();
                         group.setId(cursor.getInt(cursor.getColumnIndex(GROUP_COL_1)));
@@ -86,14 +110,75 @@ public class DBHelper extends SQLiteOpenHelper {
                         group.setStatus(cursor.getInt(cursor.getColumnIndex(GROUP_COL_3)));
                         groupList.add(group);
 
-                    }while (cursor.moveToNext());
+                    } while (cursor.moveToNext());
                 }
             }
-        }finally {
+        } finally {
             db.endTransaction();
             cursor.close();
         }
         return groupList;
     }
+
+    //    anjalee --------------
+    public void openDatabase() {
+        db = this.getWritableDatabase();
+    }
+
+    public void insertTask(ToDoModel task){
+        ContentValues cv = new ContentValues();
+        cv.put(TASK, task.getTask());
+        cv.put(STATUS, 0);
+        db.insert(TODO_TABLE, null, cv);
+    }
+
+    @SuppressLint("Range")
+    public List<ToDoModel> getAllTasks(){
+        List<ToDoModel> taskList = new ArrayList<>();
+        Cursor cur = null;
+        db.beginTransaction();
+        try{
+            cur = db.query(TODO_TABLE, null, null, null, null, null, null, null);
+            if(cur != null){
+                if(cur.moveToFirst()){
+                    do{
+                        ToDoModel task = new ToDoModel();
+                        task.setId(cur.getInt(cur.getColumnIndex(ID)));
+                        task.setTask(cur.getString(cur.getColumnIndex(TASK)));
+                        task.setStatus(cur.getInt(cur.getColumnIndex(STATUS)));
+                        taskList.add(task);
+                    }
+                    while(cur.moveToNext());
+                }
+            }
+        }
+        finally {
+            db.endTransaction();
+            assert cur != null;
+            cur.close();
+        }
+        return taskList;
+    }
+
+    public void updateStatus(int id, int status){
+        ContentValues cv = new ContentValues();
+        cv.put(STATUS, status);
+        db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
+    }
+
+    public void updateTask(int id, String task) {
+        ContentValues cv = new ContentValues();
+        cv.put(TASK, task);
+        db.update(TODO_TABLE, cv, ID + "= ?", new String[] {String.valueOf(id)});
+    }
+
+    public void deleteTask(int id){
+        db.delete(TODO_TABLE, ID + "= ?", new String[] {String.valueOf(id)});
+    }
+
+    //    anjalee --------------
+
+
+
 
 }
